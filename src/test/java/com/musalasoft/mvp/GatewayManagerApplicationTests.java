a9a64ded3b359f54b6bbef8dec37d63bd53e9c3e
Mohamed Adel <mohamed.adel@magalix.com>
2021-04-24 01:24:30 +0200

    Add README file

---
+++ src/test/java/com/musalasoft/mvp/GatewayManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musalasoft.mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
